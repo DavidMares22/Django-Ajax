2f0bf798f7e9e38baac4ad880ae6d30225ca8822
damr <damr18@gmail.com>
2020-08-29 12:50:32 -0500

    after creating, deletes rows and calls getRows

--- static_files/js/app.js
+++ static_files/js/app.js
@@ -1,40 +1,49 @@
 $(document).ready(function(){
 
-    $.ajax({
-        url:  '/rooms/list',
-        type:  'get',
-        dataType:  'json',
-        success: function  (data) {
-            let rows =  '';
-            data.rooms.forEach(room => {
-            rows += `
-            <tr>
-                <td>${room.room_number}</td>
-                <td>${room.name}</td>
-                <td>${room.nobeds}</td>
-                <td>${room.room_type}</td>
-                <td>
-                    <button class="btn btn-danger deleteBtn" data-id="${room.id}">Delete</button>
-                    <button class="btn btn-info updateBtn" data-id="${room.id}">Update</button>
-                </td>
-            </tr>`;
-        });
-        $('#myTable > tbody:last-child').append(rows);
 
-        $('.deleteBtn').on("click",function(){
-
-            deleteRoom($(this));
-        });
-    
-        
-    }
-
-        
-    });
-
+getRooms();
   
 });
- 
+
+function deleteRows(){
+
+  $("tbody").empty();
+}
+
+function getRooms(){
+  $.ajax({
+    url:  '/rooms/list',
+    type:  'get',
+    dataType:  'json',
+    success: function  (data) {
+        let rows =  '';
+        data.rooms.forEach(room => {
+        rows += `
+        <tr>
+            <td>${room.room_number}</td>
+            <td>${room.name}</td>
+            <td>${room.nobeds}</td>
+            <td>${room.room_type}</td>
+            <td>
+                <button class="btn btn-danger deleteBtn" data-id="${room.id}">Delete</button>
+                <button class="btn btn-info updateBtn" data-id="${room.id}">Update</button>
+            </td>
+        </tr>`;
+    });
+    $('#myTable > tbody:last-child').append(rows);
+
+    $('.deleteBtn').on("click",function(){
+
+        deleteRoom($(this));
+    });
+
+    
+}
+
+    
+});
+
+}
 
 function getCookie(name) {
     var cookieValue = null;
@@ -67,8 +76,8 @@ function  deleteRoom(el){
         success:  function (data) {
             // console.log(data);
             $('.message').html(data.message);
-            $(".message").fadeTo(2000, 500).slideUp(500, function(){
-                $(".message").slideUp(500);
+            $(".message").fadeTo(1000, 300).slideUp(300, function(){
+                $(".message").slideUp(300);
             });
 
             $(el).parents()[1].remove();
@@ -87,7 +96,9 @@ $("#modal-room").on("submit", ".js-room-create-form", function () {
       dataType: 'json',
       success: function (data) {
         if (data.form_is_valid) {
-          alert("Room created!");  // <-- This is just a placeholder for now for testing
+          // alert("Room created!");  // <-- This is just a placeholder for now for testing
+          deleteRows();
+          getRooms();
         }
         else {
           $("#modal-room .modal-content").html(data.html_form);
